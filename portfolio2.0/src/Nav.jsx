e6a85865bc2a6a24227ece912918da2ac26cb764
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-06-15 16:22:24 -0500

    Updated CSS, JS
    - Added more styles to menu
    - Tweaked the JSX to render menu in a different way on mobile devices

--- portfolio2.0/src/Nav.jsx
+++ portfolio2.0/src/Nav.jsx
@@ -2,22 +2,40 @@
 
 export default function Nav(props) {
 
-    const menuIcon =
-        props.showMenu ?
-        <i className="fa-solid fa-bars" onClick={props.toggleMenu}></i> :
-        <nav className="nav-container2">
-                <i className="fa-solid fa-xmark" onClick={props.toggleMenu}></i>
-                <li className="nav-item">Home</li>
-                <li className="nav-item">About</li>
-                <li className="nav-item">My Work</li>
-                <li className="nav-item">Contact</li>
-        </nav>
+    const styles = {
+        width: props.showMenu ? "100%" : "0",
+        height: props.showMenu ? "100vh" : "0",
+        overflowX: props.showMenu ? "" : "hidden"
+    }
+
+    const position = {
+        top: props.showMenu ? "10px" : "0",
+        right: props.showMenu ? "15px" : "0",
+        display: !props.showMenu && "none",
+    }
+    // const menuIcon =
+    //     props.showMenu ?
+    //     <i className="fa-solid fa-bars" onClick={props.toggleMenu}></i> :
+    //     <nav className="nav-container2" style={styles}>
+    //             <i className="fa-solid fa-xmark" onClick={props.toggleMenu}></i>
+    //             <li className="nav-item">Home</li>
+    //             <li className="nav-item">About</li>
+    //             <li className="nav-item">My Work</li>
+    //             <li className="nav-item">Contact</li>
+    //     </nav>
 
     return (
         <div className="nav-container1">
             <img src="./src/assets/selfportrait.png.png" className="nav-logo"></img>
             <p className="nav-title">Gabe Davila</p>
-            {menuIcon}
+            <i className="fa-solid fa-bars" onClick={props.toggleMenu}></i>
+            <nav className="nav-container2" style={styles}>
+                <i className="fa-solid fa-xmark" onClick={props.toggleMenu} style={position}></i>
+                <li className="nav-item">Home</li>
+                <li className="nav-item">About</li>
+                <li className="nav-item">My Work</li>
+                <li className="nav-item">Contact</li>
+            </nav>
         </div>
     )
 }
