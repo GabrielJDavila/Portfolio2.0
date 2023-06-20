b30f7aa3f19275061cc830a76eecdfc1ea182a33
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-06-20 12:08:40 -0500

    css updates

--- portfolio2.0/src/Projects.jsx
+++ portfolio2.0/src/Projects.jsx
@@ -1,16 +1,31 @@
+import data from './projectdata'
 
 export default function Projects() {
+
+    const projects = data.map((item, index) => {
+        return (
+            <a key={index} href={item.source} className="portfolio-item">
+                <img src={item.img} alt={item.alt} className="portfolio-img img1" id={item.id}></img>
+                <p className={"project-name"}>{item.name}</p>
+                <p>{item.tools}</p>
+                <p className="project-description">{item.description}</p>
+            </a>
+        )
+    })
+
     return (
         <div className="projects-container">
             <h2 className="projects-title">My projects</h2>
             <p>A selection of my range of work</p>
             <div className="mini-proj-container">
-                <a href="https://beamish-ganache-a020c1.netlify.app/" className="portfolio-item">
-                    <img src="./src/assets/tenzies-screenshot.png" alt="screenshot of tenzies app" className="portfolio-img img1"></img>
-                    <p className="project-name">Tenzies game</p>
-                    <p className="project-description">built with: React, CSS, HTML</p>
-                </a>
-                <a href="https://startling-begonia-80cfd3.netlify.app/" className="portfolio-item">
+                {projects}
+                {/* <a href={props.source} className="portfolio-item">
+                    <img src={props.img} alt={props.alt} className="portfolio-img img1"></img>
+                    <p className={"project-name"}>{props.name}</p>
+                    <p>{props.tools}</p>
+                    <p className="project-description">{props.description}</p>
+                </a> */}
+                {/* <a href="https://startling-begonia-80cfd3.netlify.app/" className="portfolio-item">
                     <img src="./src/assets/quizzical-screenshot.png" alt="screenshot of quizzical app" className="portfolio-img img2"></img>
                     <p className="project-name">Quizzical</p>
                     <p className="project-description">built with: React, CSS, HTML</p>
@@ -24,7 +39,7 @@ export default function Projects() {
                     <img src="./src/assets/endorsements-screenshot.png" alt="screenshot of endorsements app" className="portfolio-img img4"></img>
                     <p className="project-name">Employee Endorsements Page</p>
                     <p className="project-description">built with: Vanilla Javascript, Firebase, CSS, HTML</p>
-                </a>
+                </a> */}
             </div>
         </div>
     )
--- portfolio2.0/src/projectdata.jsx
+++ portfolio2.0/src/projectdata.jsx
@@ -1,18 +1,38 @@
-export const projectdata = [
+export default [
     {
+        id: "item1",
         name: "Tenzies",
-        description: "built with: React, CSS, HTML",
+        tools: "React, CSS, HTML",
+        description: "Tenzies is a game where you roll dice until all dice are matching numbers.",
         source: "https://beamish-ganache-a020c1.netlify.app/",
         img: "./src/assets/tenzies-screenshot.png",
         alt: "screenshot of tenzies app"
     },
     {
-
+        id: "item2",
+        name: "Quizzical",
+        tools: "React, Open Trivia Database API, CSS, HTML",
+        description: "Quizzical tests your knowledge of popular videogame trivia.",
+        source: "https://startling-begonia-80cfd3.netlify.app/",
+        img: "./src/assets/quizzical-screenshot.png",
+        alt: "screenshot of quizzical app"
     },
     {
-
+        id: "item3",
+        name: "Movie Watchlist",
+        tools: "Javascript, OMDb API, CSS, HTML",
+        description: "This project lets you search movies and add them to your personal watchlist.",
+        source: "https://magical-toffee-a92ae3.netlify.app/",
+        img: "./src/assets/watchlist-screenshot.png",
+        alt: "screenshot of watchlist app"
     },
     {
-
+        id: "item4",
+        name: "Employee Endorsements",
+        tools: "Javascript, Firebase, CSS, HTML",
+        description: "Endorsements lets users write an inspirational note to other coworkers. It saves data to a database and retrieves it to render.",
+        source: "https://venerable-maamoul-37b869.netlify.app/",
+        img: "./src/assets/endorsements-screenshot.png",
+        alt: "screenshot of endorsements app"
     }
 ]
