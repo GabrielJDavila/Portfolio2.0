ae8b0928260983e5cea275ed79f1713860d37aed
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-06-24 17:36:53 -0500

    updates
    - added icons for social
    - fixed nav menu to take up entire viewport when opened
    -  changed some font sizes

--- portfolio2.0/src/Hero.jsx
+++ portfolio2.0/src/Hero.jsx
@@ -2,9 +2,9 @@
 export default function Hero() {
     return (
         <div className="hero-container">
-            <img src="./src/assets/selfportrait2.png" className="hero-logo"></img>
             <h1 className="hero-title">Hi, I'm Gabriel</h1>
-            <p className="hero-description">Frontend web developer</p>
+            <img src="./src/assets/selfportrait2.png" className="hero-logo"></img>
+            <p className="hero-description">Frontend Developer</p>
         </div>
     )
 }
--- portfolio2.0/src/Nav.jsx
+++ portfolio2.0/src/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav(props) {
 
     const styles = {
         width: props.showMenu ? "100%" : "0",
-        height: props.showMenu ? "80vh" : "0",
+        height: props.showMenu ? "100vh" : "0",
         overflowX: props.showMenu ? "" : "hidden"
     }
 
@@ -13,20 +13,16 @@ export default function Nav(props) {
         // right: props.showMenu ? "15px" : "0",
         display: !props.showMenu && "none",
     }
-    // const menuIcon =
-    //     props.showMenu ?
-    //     <i className="fa-solid fa-bars" onClick={props.toggleMenu}></i> :
-    //     <nav className="nav-container2" style={styles}>
-    //             <i className="fa-solid fa-xmark" onClick={props.toggleMenu}></i>
-    //             <li className="nav-item">Home</li>
-    //             <li className="nav-item">About</li>
-    //             <li className="nav-item">My Work</li>
-    //             <li className="nav-item">Contact</li>
-    //     </nav>
 
     return (
         <div className="nav-container1">
             <p className="nav-title">Gabe Davila</p>
+            <div className="social-icons-container">
+                {/* <a href="https://www.linkedin.com/in/gabrieljdavila/" className="social-link"><img src="./src/assets/Orion_linkedin.svg" className="social-icon"></img></a>
+                <a href="https://github.com/GabrielJDavila" className="social-link"><img src="./src/assets/Orion_github.svg" className="social-icon"></img></a> */}
+            </div>
+            <a href="https://www.linkedin.com/in/gabrieljdavila/" target="_blank" alt="linkedin social icon"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://github.com/GabrielJDavila" target="_blank" alt="github social icon"><i class="fa-brands fa-github"></i></a>
             {!props.showMenu ? <i className="fa-solid fa-bars" onClick={props.toggleMenu}></i> :
             <i className="fa-solid fa-xmark" onClick={props.toggleMenu} style={position}></i>
             }
