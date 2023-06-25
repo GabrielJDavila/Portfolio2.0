6d915c5cd0fcb3261dc948bb2a137449069ef326
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-06-25 12:20:52 -0500

    Added Skill Icons
    Added icons to about section

--- portfolio2.0/src/About.jsx
+++ portfolio2.0/src/About.jsx
@@ -18,11 +18,12 @@ export default function About() {
                     </div>
                     <div className="design-container">
                         <p>Skills</p>
-                        <div>
-                            <p>React JS</p>
-                            <p>Firebase</p>
-                            <p>HTML</p>
-                            <p>CSS</p>
+                        <div className="skills-container">
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" className="skill-icon"></img>        
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" className="skill-icon"></img> 
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" className="skill-icon"></img>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" className="skill-icon"></img> 
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" className="skill-icon"></img> 
                         </div>
                     </div>
                 </div>
