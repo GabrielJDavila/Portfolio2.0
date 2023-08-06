867dcbf92cc812a096d73482772c4fcfb70bce5d
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-08-06 08:21:21 -0500

    testing file paths

--- portfolio2.0/src/Hero.jsx
+++ portfolio2.0/src/Hero.jsx
@@ -3,10 +3,10 @@ export default function Hero() {
     return (
         <div className="hero-container">
             <h1 className="hero-title">Hi, I'm Gabriel</h1>
-            <img src="./src/assets/selfportrait2.png" className="hero-logo"></img>
+            <img src="./selfportrait2.png" className="hero-logo"></img>
             <div className="hero-description">
                 <p className="hero-description-title">Frontend Developer</p>
-                <div className="skills-container">
+                <div className="skills-container" >
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" className="skill-icon"></img>        
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" className="skill-icon"></img> 
                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" className="skill-icon"></img>
--- portfolio2.0/src/projectdata.jsx
+++ portfolio2.0/src/projectdata.jsx
@@ -5,7 +5,7 @@ export default [
         tools: "React, React Router, Firebase",
         description: "Income and expense tracker for a small cleaning business. Simple and user-friendly application that has a real-world use case.",
         source: "https://gregarious-moonbeam-69026b.netlify.app/",
-        img: "/src/assets/mgcleantracker.png",
+        img: "./mgcleantracker.png",
         alt: "screenshot of mgclean tracker"
     },
     {
@@ -14,7 +14,7 @@ export default [
         tools: "React, CSS, HTML",
         description: "Tenzies is a game where you roll dice until all dice are matching numbers.",
         source: "https://beamish-ganache-a020c1.netlify.app/",
-        img: "/src/assets/tenzies-screenshot.png",
+        img: "./tenzies-screenshot.png",
         alt: "screenshot of tenzies app"
     },
     {
@@ -23,7 +23,7 @@ export default [
         tools: "React, Open Trivia Database API, CSS, HTML",
         description: "Quizzical tests your knowledge of popular videogame trivia.",
         source: "https://startling-begonia-80cfd3.netlify.app/",
-        img: "/src/assets/quizzical-screenshot.png",
+        img: "./quizzical-screenshot.png",
         alt: "screenshot of quizzical app"
     },
     {
@@ -32,7 +32,7 @@ export default [
         tools: "Javascript, OMDb API, CSS, HTML",
         description: "This project lets you search movies and add them to your personal watchlist.",
         source: "https://magical-toffee-a92ae3.netlify.app/",
-        img: "/src/assets/watchlist-screenshot.png",
+        img: "./watchlist-screenshot.png",
         alt: "screenshot of watchlist app"
     },
     {
@@ -41,7 +41,7 @@ export default [
         tools: "Javascript, Firebase, CSS, HTML",
         description: "Endorsements lets users write an inspirational note to other coworkers. It saves data to a database and retrieves it to render.",
         source: "https://venerable-maamoul-37b869.netlify.app/",
-        img: "/src/assets/endorsements-app.png",
+        img: "./endorsements-app.png",
         alt: "screenshot of endorsements app"
     }
 ]
