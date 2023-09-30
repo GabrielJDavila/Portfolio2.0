4ea7f10e7ededc441cea6013ea01365bec88c1f3
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-09-29 21:00:58 -0500

    updates to projects section
    added another project to portfolio

--- portfolio2.0/src/Projects.jsx
+++ portfolio2.0/src/Projects.jsx
@@ -5,12 +5,14 @@ export default function Projects() {
     const projects = data.map((item, index) => {
         return (
             <div key={index} className="project-item-container">
+                <h4 className="project-name">{item.name}</h4>
                 <img src={item.img} alt={item.alt} className="project-img img1" id={item.id}></img>
                 <div className="project-link-container">
                     <a href={item.source} className="project-link" target="_blank">Live site</a>
                     <p className="divider">|</p>
                     <a href={item.github} className="project-link" target="_blank">GitHub</a>
                 </div>
+                <p className="tools">Tools: {item.tools}</p>
                 <p className="project-description">{item.description}</p>
             </div>
         )
--- portfolio2.0/src/projectdata.jsx
+++ portfolio2.0/src/projectdata.jsx
@@ -1,8 +1,18 @@
 export default [
     {
         id: "item0",
+        name: "H4 Team Notes App (on going)",
+        tools: "React Router, Firestore, CSS, HTML",
+        description: "This is a team notes app for a highly successful fitness studio; it includes push notifications and dynamic rendering of information from a database. Allows users to create and share notes.",
+        source: "https://main--beautiful-starlight-72e7d4.netlify.app/",
+        github: "https://github.com/GabrielJDavila/h4teamnotes2.0",
+        img: "./h4teamnotes.png",
+        alt: "screenshot of h4app app"
+    },
+    {
+        id: "item1",
         name: "Income & Expense Tracker",
-        tools: "React, React Router, Firebase",
+        tools: "React, React Router, Firebase, CSS, HTML",
         description: "Income and expense tracker for a small cleaning business. Simple and user-friendly application that has a real-world use case.",
         source: "https://gregarious-moonbeam-69026b.netlify.app/",
         github: "https://github.com/GabrielJDavila/mgcleantracker",
@@ -10,17 +20,17 @@ export default [
         alt: "screenshot of mgclean tracker"
     },
     {
-        id: "item1",
-        name: "H4 Team Notes App (on going)",
-        tools: "React Router, Firestore, CSS, HTML",
-        description: "This is a team notes app for a highly successful fitness studio; it includes push notifications and dynamic rendering of information from Firestore. Built with React Router.",
-        source: "https://main--beautiful-starlight-72e7d4.netlify.app/",
-        github: "https://github.com/GabrielJDavila/h4teamnotes2.0",
-        img: "./h4teamnotes.png",
-        alt: "screenshot of h4app app"
+        id: "item2",
+        name: "M&G Clean Pros Website",
+        tools: "React, HTML, CSS",
+        description: "Professional website for a small cleaning business. Responsive design was the main goal, and clean coding practices.",
+        source: "https://mgcleanpros.com/",
+        github: "https://github.com/GabrielJDavila/mgcleansite",
+        img: "./mgcleanprosmobile.png",
+        alt: "screenshot of mgcleanpros website"
     },
     {
-        id: "item2",
+        id: "item3",
         name: "Quizzical",
         tools: "React, Open Trivia Database API, CSS, HTML",
         description: "Quizzical tests your knowledge of popular videogame trivia. Bring your A-game!",
@@ -30,7 +40,7 @@ export default [
         alt: "screenshot of quizzical app"
     },
     {
-        id: "item3",
+        id: "item4",
         name: "Movie Watchlist",
         tools: "Javascript, OMDb API, CSS, HTML",
         description: "This project lets you search movies and add them to your personal watchlist.",
@@ -40,7 +50,7 @@ export default [
         alt: "screenshot of watchlist app"
     },
     {
-        id: "item4",
+        id: "item5",
         name: "Employee Endorsements",
         tools: "Javascript, Firebase, CSS, HTML",
         description: "Endorsements lets users write an inspirational note to other coworkers. It saves data to a database and retrieves it to render.",
