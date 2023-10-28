aa07a2cd81967b86fe1759114a1f6a80da9c48f8
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-10-27 19:04:18 -0500

    Updated images for projects

--- portfolio2.0/src/projectdata.jsx
+++ portfolio2.0/src/projectdata.jsx
@@ -6,7 +6,7 @@ export default [
         description: "This is a team notes app for a highly successful fitness studio; it includes push notifications and dynamic rendering of information from a database. Allows users to create and share notes.",
         source: "https://h4teamnotes.netlify.app",
         github: "https://github.com/GabrielJDavila/h4teamnotes2.0",
-        img: "./h4teamnotes.png",
+        img: "./h4teamnotesscreenshot.png",
         alt: "screenshot of h4app app"
     },
     {
@@ -16,7 +16,7 @@ export default [
         description: "Income and expense tracker for a small cleaning business. Simple and user-friendly application that has a real-world use case.",
         source: "https://mgbudgettracker.netlify.app/",
         github: "https://github.com/GabrielJDavila/mgcleantracker",
-        img: "./mgcleantracker.png",
+        img: "./mgbudgettrackerscreenshot.png",
         alt: "screenshot of mgclean tracker"
     },
     {
@@ -26,7 +26,7 @@ export default [
         description: "Professional website for a small cleaning business. Responsive design was the main goal, and clean coding practices.",
         source: "https://mgcleanpros.com/",
         github: "https://github.com/GabrielJDavila/mgcleansite",
-        img: "./mgcleanprosmobile.png",
+        img: "./mgcleanprosscreenshot.png",
         alt: "screenshot of mgcleanpros website"
     },
     {
@@ -36,7 +36,7 @@ export default [
         description: "Quizzical tests your knowledge of popular videogame trivia. Bring your A-game!",
         source: "https://startling-begonia-80cfd3.netlify.app/",
         github: "https://github.com/GabrielJDavila/Quizzical",
-        img: "./quizzical-screenshot2.png",
+        img: "./quizzicalscreenshot.png",
         alt: "screenshot of quizzical app"
     },
     {
@@ -44,19 +44,19 @@ export default [
         name: "Movie Watchlist",
         tools: "Javascript, OMDb API, CSS, HTML",
         description: "This project lets you search movies and add them to your personal watchlist.",
-        source: "https://magical-toffee-a92ae3.netlify.app/",
+        source: "https://gabemoviewatchlist.netlify.app",
         github: "https://github.com/GabrielJDavila/movieWatchlist",
-        img: "./watchlist-screenshot.png",
+        img: "./moviewatchlistscreenshot.png",
         alt: "screenshot of watchlist app"
     },
     {
         id: "item5",
-        name: "Employee Endorsements",
-        tools: "Javascript, Firebase, CSS, HTML",
-        description: "Endorsements lets users write an inspirational note to other coworkers. It saves data to a database and retrieves it to render.",
-        source: "https://venerable-maamoul-37b869.netlify.app/",
-        github: "https://github.com/GabrielJDavila/Employee-Endorsement-Project",
-        img: "./endorsements-app.png",
-        alt: "screenshot of endorsements app"
+        name: "Invoice Creator",
+        tools: "React, Javascript, CSS, HTML",
+        description: "This is a simple invoice creator made with React. This project was made with the intention of gaining more familiarity with the basics of React (state, useEffect, props, etc).",
+        source: "https://sampleinvoicecreator.netlify.app",
+        github: "https://github.com/GabrielJDavila/InvoiceCreator",
+        img: "./invoicecreatorscreenshot.png",
+        alt: "screenshot of invoice creator app"
     }
 ]
