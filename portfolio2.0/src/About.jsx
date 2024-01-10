0310fb9979cecde42083dabd527937621d7b82c4
Gabriel Davila <107373187+GabrielJDavila@users.noreply.github.com>
2024-01-10 13:15:55 -0600

    Update About.jsx

--- portfolio2.0/src/About.jsx
+++ portfolio2.0/src/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
                 <h2 className="about-title">Who I am</h2>    
                 <p className="who-i-am-description">I'm a frontend web developer based out of Illnois, USA. I have caught the programming bug, and love creating web apps that solve real world problems and are fun to use!<br></br><br></br>
                 I specialize in React JS, and have a great understanding of the fundamentals of web dev - HTML, CSS, and Javascript, and have experience with many other technologies as well.<br></br><br></br>
-                I have a huge interest in fitness and technology; previously I've been in the fitness industry for 7+ years, and am especially interested in the intersection of tech, health, and sustainability.</p>
+                I have a HUGE interest in fitness and technology; previously I've been in the fitness industry for 7+ years, and am especially interested in the intersection of tech, health, and sustainability.</p>
             </div>
         </main>
     )
